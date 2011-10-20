fd60b4964e921c691e5a397a5605c95b3b0a38b3
Sean Fisk <sean@seanfisk.com>
2011-10-20 16:35:48 -0400

    Improve code comment.

--- graph_generator.py
+++ graph_generator.py
@@ -4,7 +4,7 @@ import time
 # generate the reverse complement of a sequence
 def reverse_complement(sequence):
     rc = ""
-    # iterate on string from beginning to end_time (I think?)
+    # iterate on string from beginning to end (I think?)
     for i in sequence[::-1]:
         if i == 'A':
             rc = rc + 'T'
@@ -21,7 +21,7 @@ def reverse_complement(sequence):
 
 # check arguments
 if len(sys.argv) < 5:
-    print "Expected format: graph_generator.py <k> <input_file> <output_file> (<read_count>)"
+    print "Expected format: graph_generator.py <k> <input_file> <output_file> (<num_lines_to_read>)"
     exit
     
 # open input file
@@ -32,15 +32,16 @@ output_file = open(sys.argv[3], "w")
 # initialize k from the first argument, convert to an integer
 k = int(sys.argv[1])
 
-# set read_count to the argument if it'subsequence been given, otherwise default to -1
+# set num_lines_to_read to the argument if it'subsequence been given, otherwise default to -1
+# num_lines_to_read denotes the amount of sequence lines to read from the file
 if len(sys.argv) == 5:
-    read_count = int(sys.argv[4]);
+    num_lines_to_read = int(sys.argv[4]);
 else:
-    read_count = -1
+    num_lines_to_read = -1
     
-# initialize kmer array
+# initialize kmer hash
 k_mers = {}
-overlap_index = list()
+# overlap_index = list() # seems unused
 
 overlap_count = 0
 k_mer_index = 0
@@ -49,18 +50,19 @@ k_mer_count = 0
 
 # assume all reads are same length as first read
 line = input_file.readline()
-# remove whitespace from beginning and end_time of string so length is accurate
+# remove whitespace from beginning and end of string so length is accurate
 line = line.strip()
 read_length = len(line)
 
-input_file.seek(0) # start_time reading at beginning of file again
+input_file.seek(0) # start reading at beginning of file again
 
 # fields to indicate the length of things
-output_file.write(str(read_count) + "," + str(read_length) + "," + str(read_length - k + 1) + "\n")
-# start_time timing
+# read_length - k + 1 : number of k-mer per read
+output_file.write(str(num_lines_to_read) + "," + str(read_length) + "," + str(read_length - k + 1) + "\n")
+# start timing
 start_time = time.time()
 for line in input_file:
-    # remove whitespace from beginning and end_time of string
+    # remove whitespace from beginning and end of string
     line = line.strip()
     # get the reverse complement, see above
     rc = reverse_complement(line)
@@ -72,7 +74,7 @@ for line in input_file:
         # grab k-mer substring from end_time of reverse complement
         reverse_subsequence = rc[read_length - k - i : read_length - i]
         
-        # if this k-mer has already been added
+        # check if the k-mer overlaps with an existing k-mer
         if(subsequence[:-1] in k_mers):
             overlap_count = overlap_count + 1
             output_file.write(str(read_index) + "," + str(i) + ","  + subsequence + "," + reverse_subsequence +"," +  str(k_mers[subsequence[:-1]][0]) + "," + str(k_mers[subsequence[:-1]][1]) + "\n")
@@ -88,9 +90,9 @@ for line in input_file:
             
     # advance indices
     read_index = read_index + 1
-    read_count = read_count - 1
-    # quit when read_count is zero
-    if(read_count == 0):
+    num_lines_to_read = num_lines_to_read - 1
+    # quit when num_lines_to_read is zero
+    if(num_lines_to_read == 0):
         break
     
 # stop timing
