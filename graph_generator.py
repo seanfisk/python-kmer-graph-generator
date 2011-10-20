637f227f885f640ae42f5774f8b37d18911213b9
Sean Fisk <sean@seanfisk.com>
2011-10-20 14:56:54 -0400

    Add more annotations, make variable names more verbose.

--- graph_generator.py
+++ graph_generator.py
@@ -2,9 +2,9 @@ import sys
 import time
 
 # generate the reverse complement of a sequence
-def reverse_comp(s):
+def reverse_complement(s):
     rc = ""
-    # iterate on string from beginning to end (I think?)
+    # iterate on string from beginning to end_time (I think?)
     for i in s[::-1]:
         if i == 'A':
             rc = rc + 'T'
@@ -23,16 +23,16 @@ def reverse_comp(s):
 if len(sys.argv) < 5:
     print "Expected format: graph_generator.py <k> <input_file> <output_file> (<read_count>)"
     exit
-
+    
 # open input file
-fr = open(sys.argv[2], "r")
+input_file = open(sys.argv[2], "r")
 # open output file
-fw = open(sys.argv[3], "w")
+output_file = open(sys.argv[3], "w")
 
 # initialize k from the first argument, convert to an integer
 k = int(sys.argv[1])
 
-# set read_count to the argument if it's been given, otherwise default to -1
+# set read_count to the argument if it'subsequence been given, otherwise default to -1
 if len(sys.argv) == 5:
     read_count = int(sys.argv[4]);
 else:
@@ -40,57 +40,64 @@ else:
     
 # initialize kmer array
 k_mers = {}
-overlap_idx = list()
+overlap_index = list()
 
 overlap_count = 0
-k_mer_idx = 0
-read_idx = 0
+k_mer_index = 0
+read_index = 0
 k_mer_count = 0
 
 # assume all reads are same length as first read
-line = fr.readline()
-# remove whitespace from beginning and end of string so length is accurate
+line = input_file.readline()
+# remove whitespace from beginning and end_time of string so length is accurate
 line = line.strip()
-r_len = len(line)
+read_length = len(line)
 
-fr.seek(0) # start reading at beginning of file again
+input_file.seek(0) # start_time reading at beginning of file again
 
 # fields to indicate the length of things
-fw.write(str(read_count) + "," + str(r_len) + "," + str(r_len - k + 1) + "\n")
-# start timing
-start = time.time()
-for line in fr:
-    # remove whitespace from beginning and end of string
+output_file.write(str(read_count) + "," + str(read_length) + "," + str(read_length - k + 1) + "\n")
+# start_time timing
+start_time = time.time()
+for line in input_file:
+    # remove whitespace from beginning and end_time of string
     line = line.strip()
     # get the reverse complement, see above
-    rc = reverse_comp(line)
+    rc = reverse_complement(line)
     
-    for i in range(r_len - k + 1):
-        s = line[i : k + i]
-        rs = rc[r_len - k - i : r_len - i]
+    # iterate over all k-mers in the sequence
+    for i in range(read_length - k + 1):
+        # grab k-mer substring from front of line
+        subsequence = line[i : k + i]
+        # grab k-mer substring from end_time of reverse complement
+        reverse_subsequence = rc[read_length - k - i : read_length - i]
         
-        if(s[:-1] in k_mers):
+        # if this k-mer has already been added
+        if(subsequence[:-1] in k_mers):
             overlap_count = overlap_count + 1
-            fw.write(str(read_idx) + "," + str(i) + ","  + s + "," + rs +"," +  str(k_mers[s[:-1]][0]) + "," + str(k_mers[s[:-1]][1]) + "\n")
+            output_file.write(str(read_index) + "," + str(i) + ","  + subsequence + "," + reverse_subsequence +"," +  str(k_mers[subsequence[:-1]][0]) + "," + str(k_mers[subsequence[:-1]][1]) + "\n")
         else:
-            fw.write(str(read_idx) + "," + str(i) + ","  + s + "," + rs +","+ str(read_idx) + "," + str(i) + "\n")
+            output_file.write(str(read_index) + "," + str(i) + ","  + subsequence + "," + reverse_subsequence +","+ str(read_index) + "," + str(i) + "\n")
             
-        if not (s[1:] in k_mers):
-            k_mers[s[1:]] = [read_idx, i]
-            k_mers[rs[1:]] = [read_idx, i]
+        # k-mer is not in the list
+        if not (subsequence[1:] in k_mers):
+            # add it to the list
+            k_mers[subsequence[1:]] = [read_index, i]
+            k_mers[reverse_subsequence[1:]] = [read_index, i]
             k_mer_count = k_mer_count + 2
             
-            
-    read_idx = read_idx + 1
+    # advance indices
+    read_index = read_index + 1
     read_count = read_count - 1
+    # quit when read_count is zero
     if(read_count == 0):
         break
     
 # stop timing
-end = time.time()
+end_time = time.time()
 
 # print timing
-print "Determine overlap time:", end - start
+print "Determine overlap time:", end_time - start_time
 
 # print results
 print "Overlapping reads: ", overlap_count
@@ -100,5 +107,5 @@ print "Number of unique " +str(k) + "-mers: ", k_mer_count
 k_mers.clear()
 
 # close files
-fr.close()
-fw.close()
+input_file.close()
+output_file.close()
